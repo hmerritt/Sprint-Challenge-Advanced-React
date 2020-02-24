d606b1d551e9f5a4e04eadd5a1f9c3939ddaa4e1
Harry Merritt <harry@merritt.es>
2020-02-24 03:20:35 +0000

    Added area-chart displaying search numbers across all players

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,12 @@
 import React, { useState, useEffect } from "react";
 import Header from "./components/header/header";
+import Chart from "./components/chart";
 import ListPlayers from "./components/listPlayers";
 import { Container } from "@material-ui/core";
 import { useLocalStorage } from "./hooks/useLocalStorage";
 import "./App.css";
 
 function App() {
-
     //  Attempt to load players from local storage
     const [players, setPlayers] = useLocalStorage("players", []);
 
@@ -16,6 +16,7 @@ function App() {
 
             <div className="content">
                 <Container>
+                    <Chart players={players} />
                     <ListPlayers players={players} setPlayers={setPlayers} />
                 </Container>
             </div>
